ce0bf3c68f29cfeee747c42009de314fb1aa1708
gustavo hanke <gustavo.hanke1@outlook.com>
2023-10-25 15:02:15 -0300

    added the transfer helper function added the interface, and the helper function for creation help

--- conta.hpp
+++ conta.hpp
@@ -1,12 +1,23 @@
 #include <mutex>
 class bank_account
 {
+public:
+    virtual float get_balance()=0;
+    virtual void deposit(float amount)=0;
+    virtual void withdraw(float amount)=0;
+};
+class bank_account_safe: public bank_account
+{
 private:
     float balance;
     std::mutex account_mutex;
 
 public:
     bank_account(float balance = 0) : balance(balance) {}
+    ~bank_account_safe()
+    {
+        delete this;
+    }
     float get_balance()
     {
         account_mutex.lock(); // evitar leitura de valores inconsistentes
@@ -34,12 +45,16 @@ public:
     }
 };
 
-class bank_account_not_safe
+class bank_account_not_safe: public bank_account
 {
 private:
     float balance;
 
 public:
+~bank_account_not_safe()
+{
+    delete this;
+}
     bank_account_not_safe(float balance = 0) : balance(balance) {}
     float get_balance()
     {
@@ -56,3 +71,16 @@ public:
             balance += amount;
     }
 };
+void transfer(bank_account &c1, bank_account &c2, float amount)
+{
+    c1.withdraw(amount);
+    c2.deposit(amount);
+}
+bank_account &create_safe_account(float initial_amount=0.0)
+{
+    return &(new bank_account_safe(float initial_amount));
+}
+bank_account &create_not_safe_account(float initial_amount=0.0)
+{
+    return &(new bank_account_not_safe(float initial_amount));
+}
